b59203160560c3e47614cae0a83a5c97b8bac2f4
zd4y <59004538+zd4y@users.noreply.github.com>
2022-08-20 18:02:46 -0500

    move cmds and messages to its own file

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,7 @@ package main
 import (
 	"fmt"
 	"io/fs"
-	"io/ioutil"
 	"os"
-	"os/exec"
-
-	"github.com/zd4y/notes/pkg/notes"
 
 	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbles/textinput"
@@ -91,54 +87,6 @@ func (m *model) toNewNote() {
 
 func (m *model) toNote(inIndex int) {
 	m.chosen = inIndex
-}
-
-type editorFinishedMsg struct{ err error }
-
-func openEditor() tea.Cmd {
-	editor := os.Getenv("EDITOR")
-	if editor == "" {
-		editor = "vim"
-	}
-	c := exec.Command(editor)
-	return tea.ExecProcess(c, func(err error) tea.Msg {
-		return editorFinishedMsg{err}
-	})
-}
-
-type verifyPasswordMsg struct {
-	passwordsMatch bool
-	err error
-}
-
-func verifyPassword(password string) tea.Cmd {
-	return func() tea.Msg {
-		err := notes.VerifyPassword(password)
-		return verifyPasswordMsg { err == nil, err }
-	}
-}
-
-type openNoteMsg struct {
-	note string
-	err error
-}
-
-func openNote(notePath string, password string) tea.Cmd {
-	return func() tea.Msg {
-		note, err := notes.OpenNote(notePath, password)
-		return openNoteMsg{note, err}
-	}
-}
-
-type dirFilesMsg struct { files []fs.FileInfo }
-
-func getDirFiles() tea.Msg {
-	files, err := ioutil.ReadDir("./")
-	if err != nil {
-		fmt.Println("fatal: ", err)
-		os.Exit(1)
-	}
-	return dirFilesMsg{files}
 }
 
 func (m model) Init() tea.Cmd {
---
+++ cmd/messages.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"os/exec"
+
+	"github.com/zd4y/notes/pkg/notes"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type editorFinishedMsg struct{ err error }
+
+func openEditor() tea.Cmd {
+	editor := os.Getenv("EDITOR")
+	if editor == "" {
+		editor = "vim"
+	}
+	c := exec.Command(editor)
+	return tea.ExecProcess(c, func(err error) tea.Msg {
+		return editorFinishedMsg{err}
+	})
+}
+
+type verifyPasswordMsg struct {
+	passwordsMatch bool
+	err error
+}
+
+func verifyPassword(password string) tea.Cmd {
+	return func() tea.Msg {
+		err := notes.VerifyPassword(password)
+		return verifyPasswordMsg { err == nil, err }
+	}
+}
+
+type openNoteMsg struct {
+	note string
+	err error
+}
+
+func openNote(notePath string, password string) tea.Cmd {
+	return func() tea.Msg {
+		note, err := notes.OpenNote(notePath, password)
+		return openNoteMsg{note, err}
+	}
+}
+
+type dirFilesMsg struct { files []fs.FileInfo }
+
+func getDirFiles() tea.Msg {
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+		fmt.Println("fatal: ", err)
+		os.Exit(1)
+	}
+	return dirFilesMsg{files}
+}
+
